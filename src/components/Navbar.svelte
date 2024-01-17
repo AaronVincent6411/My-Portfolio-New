6def7c48e94e5f90a4c6fe86d73863838e375622
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2024-01-17 23:19:52 +0530

    created navbar and added icon

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,25 @@
+<script>
+    import backicon from "../assets/Caret left.svg";
+    import logo from "../assets/me.jpg";
+    import { fade } from "svelte/transition";
+    import {  quintIn } from "svelte/easing";
+</script>
+
+<nav id="navbar" class="w-full h-24 fixed p-5 px-10 navbar" in:fade={{ easing:quintIn }} out:fade={{ easing:quintIn }}>
+    <a href="/" class="flex flex-row">
+        <img src={backicon} alt="backicon">
+        <h2 class="back relative text-white">Back</h2>
+        <img src={logo} alt="icon" class="logo w-12 h-12 right-16 fixed rounded-full "/>
+    </a>
+</nav>
+
+<style>
+    .navbar {
+        background: linear-gradient(180deg, #000 0%, #1E1E1E 100%);
+    }
+
+    .back {
+        font-family: 'Gurajada', sans-serif;
+        font-size: 40px;
+    }
+</style>
