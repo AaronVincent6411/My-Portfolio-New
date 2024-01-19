85b3bbc065bf768afb3722e84b7d601911ebd4ac
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2024-01-19 17:09:47 +0530

    updated about section

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,24 +6,24 @@
 </script>
 
 <Navbar />
-<section id="about" class="w-full h-full top-16 relative flex flex-col justify-center text-center text-white sm:p-16 p-8 " in:fade={{ easing:quintIn }} out:fade={{ easing:quintIn }}>
+<section id="about" class="w-full h-full top-16 relative flex flex-col justify-center text-center text-white sm:p-16 p-8 select-none " in:fade={{ easing:quintIn }} out:fade={{ easing:quintIn }}>
     <h2 class="font-medium about-name">About</h2>
-    <p class="about-desc mt-4 text-justify font-normal ">I am Aaron P Laju, Final-year Computer Science (AI) student at Adi Shankara Institute Of Engineering and Technology, Kalady. Currently serving as the Community Lead of IEDC ASIET. I bring expertise as a former Technical Lead at Hackclub and Technical Co-Lead at Computer Society of India in my college. Proficient in React and Python. I specialize in Machine Learning. My portfolio includes impactful projects in ML and Web Development, demonstrating my versatility. I also engage in poster designing, showcasing my creative skills alongside my technical expertise. I actively participate in code debugging and competitive coding competitions which will help sharpen my problem-solving skills.</p>
-    <div class="sm:w-40 w-32 hover:sm:w-101 hover:w-60 sm:h-44 h-40 hover:sm:h-44 hover:h-60 relative bg-black mt-10 hover:xl:left-[40%] hover:lg:left-[30%] hover:md:left-[20%] hover:sm:left-[10%] hover:left-[12%] lg:left-[44.8%] md:left-[42%] sm:left-[40%] left-[25%] rounded-[20px] overflow-hidden transition-all duration-500 group">
-        <img src={aboutprofile} alt="about-profile" class="sm:w-32 sm:h-32 h-20 w-20 object-center object-cover rounded-full transition-all duration-500 delay-500 transform absolute sm:top-7 sm:left-4 top-10 left-6 hover:sm:top-7 hover:sm:left-4 hover:top-2 hover:left-10">
-        <div class="flex flex-col opacity-0 group-hover:opacity-100 transition-opacity duration-500 delay-0 sm:top-0 top-1/2 sm:ml-20 ml-16">
-        <div class="flex flex-row gap-10 relative left-1/3">
-            <h1>Age</h1>
-            <h1 class="ml-4">21</h1>
-        </div>
-        <div class="flex flex-row gap-10 relative left-1/3">
-            <h1>Role</h1>
-            <h1 class="ml-2">Developer</h1>
-        </div>
-        <div class="flex flex-row gap-10 relative left-1/3">
-            <h1>Place</h1>
-            <h1>Kerala</h1>
-        </div>
+    <p class="about-desc mt-4 text-justify sm:indent-20 indent-8 font-normal ">I am Aaron P Laju, Final-year Computer Science (AI) student at <a href="https://adishankara.ac.in/" target="_blank" class=" underline decoration-red-600 sm:decoration-[3px] decoration-2">Adi Shankara Institute Of Engineering and Technology</a>, Kalady. Currently serving as the Community Lead of <a href="https://iedc.adishankara.ac.in/" target="_blank" class=" underline decoration-red-600 sm:decoration-[3px] decoration-2"> IEDC ASIET</a>. I bring expertise as a former Technical Lead at <a href="https://asiet.hackclub.com/" target="_blank" class=" underline decoration-red-600 sm:decoration-[3px] decoration-2"> Hackclub</a> and Technical Co-Lead at Computer Society of India in my college. Proficient in React and Python. I specialize in Machine Learning. My portfolio includes impactful projects in ML and Web Development, demonstrating my versatility. I also engage in poster designing, showcasing my creative skills alongside my technical expertise. I actively participate in code debugging and competitive coding competitions which will help sharpen my problem-solving skills.</p>
+    <div class="sm:w-40 w-32 hover:sm:w-101 hover:w-60 sm:h-44 h-40 hover:sm:h-44 hover:h-60 relative bg-black mt-10 hover:xl:left-[40%] hover:lg:left-[30%] hover:md:left-[20%] hover:sm:left-[10%] hover:left-[12%] xl:left-[46%] lg:left-[44.8%] md:left-[42%] sm:left-[40%] left-[25%] rounded-[20px] overflow-hidden flex sm:flex-col flex-row transition-all duration-500 group">
+        <img src={aboutprofile} alt="about-profile" class="sm:w-32 sm:h-32 h-20 w-20 object-center object-cover rounded-full transition-all sm:duration-500 duration-300 delay-500 transform absolute sm:top-7 sm:left-4 top-10 left-6 group-hover:sm:top-7 group-hover:sm:left-4 group-hover:top-4 group-hover:left-20">
+        <div class="flex flex-col opacity-0 group-hover:opacity-100 transition-opacity duration-500 delay-0 sm:top-0 sm:mt-0 mt-6 sm:ml-20 ml-4">
+            <div class="flex flex-row gap-10 relative sm:left-1/3 left-4 sm:top-0 top-1/3">
+                <h1>Age</h1>
+                <h1 class="ml-4">21</h1>
+            </div>
+            <div class="flex flex-row gap-10 relative sm:left-1/3 left-4 sm:top-0 top-1/3">
+                <h1>Role</h1>
+                <h1 class="ml-2">Developer</h1>
+            </div>
+            <div class="flex flex-row gap-10 relative sm:left-1/3 left-4 sm:top-0 top-1/3">
+                <h1>Place</h1>
+                <h1>Kerala</h1>
+            </div>
         </div>
     </div>      
 </section>
@@ -55,9 +55,8 @@
     }
 
     @media screen and (max-width: 480px) {
-        /* h1 {
-            font-size: 16px;
-        } */
-
+        h1 {
+            font-size: 32px;
+        }
     }
 </style>
