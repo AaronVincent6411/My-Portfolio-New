4d25c654f86b93a8a2fa73e8e3814a75f47c9d5c
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2024-01-23 22:30:10 +0530

    created projects and added projects data

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,39 @@
+<script>
+    import Navbar from '../../components/Navbar.svelte';
+    import { projectsdata } from './projectsdata.js';
+    import githubicon from '../../assets/Github-icon.svg'
+    import {fade} from "svelte/transition";
+    import {quintIn} from "svelte/easing";
+</script>
+
+<Navbar />
+<section id="projects" class="w-full h-full top-16 relative text-center text-white sm:p-16 p-8 flex flex-col custom-scrollbar" in:fade={{ easing:quintIn }} out:fade={{ easing:quintIn }}>
+    <h2 class="font-medium projects-name select-none">Projects</h2>
+    <div class="w-full flex gap-8 relative flex-wrap mt-16">
+        {#each projectsdata as { name, url, github }, i}
+            <div class="w-101">
+                <div class="w-100 h-72 flex-col bg-black rounded-[20px]">
+                    <h1 class="projects-data-name p-2 select-none">{name}</h1>
+                    <img src={url} alt={i + 1} class="object-contain px-2 rounded-b-[20px]">
+                </div>
+                <div class="w-16 h-16 bg-black rounded-full relative left-82 -top-10">
+                    <a href={github} target="_blank">
+                        <img src={githubicon} alt={i+1} class="p-2">
+                    </a>
+                </div>
+            </div>
+        {/each}
+    </div>
+</section>
+
+<style>
+    .projects-name {
+        font-family: 'Jacques Francois', serif;
+        font-size: 72px;
+    }
+
+    .projects-data-name {
+        font-family: 'Gurajada', sans-serif;
+        font-size: 32px;
+    }
+</style>
---
+++ src/routes/projects/projectsdata.js
@@ -0,0 +1,102 @@
+export const projectsdata = [
+    {
+        name:"My Portfolio (New)",
+        url:"https://live.staticflickr.com/65535/53474861853_b7b27b1960_k.jpg",
+        github:"https://github.com/AaronVincent6411/My-Portfolio-New",
+    },
+    {
+        name:"IEDC ASIET Website",
+        url:"https://live.staticflickr.com/65535/53475144635_77a33454d8_k.jpg",
+        github:"https://github.com/IEDCASIET/IEDC-Website",
+    },
+    {
+        name:"Calendar App",
+        url:"https://live.staticflickr.com/65535/53474717671_9666e1918c_k.jpg",
+        github:"https://github.com/AdityaNikhil2828/calendar-app",
+    },
+    {
+        name:"Rock Paper Scissor CNN",
+        url:"https://live.staticflickr.com/65535/53474851558_bf937d2fbc_k.jpg",
+        github:"https://github.com/AaronVincent6411/Rock-Paper-Scissor-CNN",
+    },
+    {
+        name:"Aswamedha 2023",
+        url:"https://live.staticflickr.com/65535/53474996874_00bd7c3d48_k.jpg",
+        github:"https://github.com/AaronVincent6411/Ashwamedha23",
+    },
+    {
+        name:"Demo Calendar App",
+        url:"https://live.staticflickr.com/65535/53474670121_9ca99576a4_k.jpg",
+        github:"https://github.com/AaronVincent6411/Calendar-App-demo",
+    },
+    {
+        name:"Litter Detection",
+        url:"https://live.staticflickr.com/65535/52803998791_92acbec538_k.jpg",
+        github:"https://github.com/AaronVincent6411/Litter-Detection",
+    },
+    {
+        name:"Image Stegnography",
+        url:"https://live.staticflickr.com/65535/53474989839_48d33293fe_k.jpg",
+        github:"https://github.com/AaronVincent6411/Image-steganography",
+    },
+    {
+        name:"Audio Stegnography",
+        url:"https://live.staticflickr.com/65535/53473506176_42011b73b0_k.jpg",
+        github:"https://github.com/AaronVincent6411/audio-steganography",
+    },
+    {
+        name:"Hack Club ASIET Website",
+        url:"https://live.staticflickr.com/65535/53475149950_0f34874026_k.jpg",
+        github:"https://github.com/KKHaridev/hc_ASIET",
+    },
+    {
+        name:"My Portfolio (Old)",
+        url:"https://live.staticflickr.com/65535/53474640226_89702de8d3_k.jpg",
+        github:"https://github.com/AaronVincent6411/My-Portfolio",
+    },
+    {
+        name:"Rain Prediction using ANN",
+        url:"https://live.staticflickr.com/65535/53474624871_c124d800a6_k.jpg",
+        github:"https://github.com/AaronVincent6411/Machine-Learning-Lab/blob/main/Practice/ANN-Rain-Prediction.ipynb",
+    },
+    {
+        name:"Basic Flutter App",
+        url:"https://live.staticflickr.com/65535/53474773553_2096d188b4_k.jpg",
+        github:"https://github.com/AaronVincent6411/First-Flutter-App",
+    },
+    {
+        name:"Kathakali Detection",
+        url:"https://live.staticflickr.com/65535/53473920770_c5568817d0_k.jpg",
+        github:"https://github.com/AaronVincent6411/Project-Kathakali",
+    },
+    {
+        name:"Python Snake Game",
+        url:"https://live.staticflickr.com/65535/53473710402_e98b2cf0ab_k.jpg",
+        github:"https://github.com/AaronVincent6411/Snake-Game",
+    },
+    {
+        name:"Image Generation using React",
+        url:"https://live.staticflickr.com/65535/53474753413_71ee3fcadd_k.jpg",
+        github:"https://github.com/AaronVincent6411/Image-Generation-React.js",
+    },
+    {
+        name:"Geeky Coders",
+        url:"https://live.staticflickr.com/65535/53473538791_8b4fcc25e5_o.png",
+        github:"https://github.com/AaronVincent6411/Geeky-Coders",
+    },
+    {
+        name:"SmartC",
+        url:"https://live.staticflickr.com/65535/53473920765_4236fca7d2_o.png",
+        github:"https://github.com/AaronVincent6411/SmartC",
+    },
+    {
+        name: "Vaccination",
+        url: "https://live.staticflickr.com/65535/53473920735_dec9c4ce9b_o.png",
+        github:"https://github.com/AaronVincent6411/Vaccination",
+    },
+    {
+        name:"Supermarket",
+        url:"https://live.staticflickr.com/65535/53472597482_e97b509931_o.png",
+        github:"https://github.com/AaronVincent6411/Supermarket",
+    },
+];
