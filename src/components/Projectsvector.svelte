e38485a72d4124d13d2d93fbeaab0ddb4d5721ea
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2024-01-27 23:46:06 +0530

    created project icon animation for background

---
+++ src/components/Projectsvector.svelte
@@ -0,0 +1,60 @@
+<script>
+    export let x;
+    export let y;
+  
+    const size = Math.random() * (50 - 10) + 20;
+</script>
+  
+<div class="projectsvectoricon projectsvectoricon-1" style="left: {x}px; top: {y}px; width: {size}px; height: {size}px;"></div>
+<div class="projectsvectoricon projectsvectoricon-2" style="left: {x}px; top: {y}px; width: {size}px; height: {size}px;"></div>
+  
+<style>
+    .projectsvectoricon {
+        display: inline-block;
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        position: fixed;
+        opacity: 50%;
+    }
+
+    .projectsvectoricon.projectsvectoricon-1 {
+        left: 100px;
+        top: 100px;
+        animation: move-projectsvectoricon-1 5s infinite;
+    }
+
+    .projectsvectoricon.projectsvectoricon-2 {
+        left: -100px;
+        top: -100px;
+        animation: move-projectsvectoricon-2 5s infinite;
+    }
+
+    @keyframes move-projectsvectoricon-1 {
+        0%, 100% {
+            transform: translate(0, 0);
+        }
+        50% {
+            transform: translate(-100px, 100px);
+        }
+    }
+
+    @keyframes move-projectsvectoricon-2 {
+        0%, 100% {
+            transform: translate(80px, 80px); 
+        }
+        50% {
+            transform: translate(180px, -180px); 
+        }
+    }
+
+    .projectsvectoricon.projectsvectoricon-1 {
+        background: url(src/assets/Vector.svg) no-repeat center center; 
+        background-size: contain;
+    }
+
+    .projectsvectoricon.projectsvectoricon-2 {
+        background: url(src/assets/Vector.svg) no-repeat center center; 
+        background-size: contain;
+    }
+</style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -4,21 +4,41 @@
     import githubicon from '../../assets/Github-icon.svg'
     import {fade} from "svelte/transition";
     import {quintIn} from "svelte/easing";
+    import Projectsvector from '../../components/Projectsvector.svelte';
+    import {onMount} from 'svelte'
+
+    let projectsvectoricon = [];
+
+    let numberOfprojectsvectoricon = 70;
+ 
+    function generateRandomPosition() {
+    return {
+        x: Math.random() * (window.innerWidth-30),
+        y: Math.random() * (window.innerHeight-60),
+    };
+    }
+
+    onMount(() => {
+    projectsvectoricon = Array(numberOfprojectsvectoricon).fill().map(() => generateRandomPosition());
+    });
 </script>
 
 <Navbar />
-<section id="projects" class="w-full h-full top-16 relative text-center text-white sm:p-16 p-8 flex flex-col custom-scrollbar" in:fade={{ easing:quintIn }} out:fade={{ easing:quintIn }}>
+<section id="projects" class="w-full h-full top-16 relative flex flex-col justify-center text-center text-white lg:p-16 md:p-4 p-8" in:fade={{ easing:quintIn }} out:fade={{ easing:quintIn }}>
+    {#each projectsvectoricon as { x, y }, i (i)}
+        <Projectsvector x={x} y={y} key={i} />
+    {/each}
     <h2 class="font-medium projects-name select-none">Projects</h2>
-    <div class="w-full flex gap-8 relative flex-wrap mt-16">
+    <div class="w-full flex xl:gap-8 relative justify-center flex-wrap sm:mt-16 mt-8">
         {#each projectsdata as { name, url, github }, i}
-            <div class="w-101">
-                <div class="w-100 h-72 flex-col bg-black rounded-[20px]">
+            <div class="xl:w-101 w-96">
+                <div class="xl:w-100 w-80 xl:h-72 h-60 flex-col bg-black rounded-[20px]">
                     <h1 class="projects-data-name p-2 select-none">{name}</h1>
                     <img src={url} alt={i + 1} class="object-contain px-2 rounded-b-[20px]">
                 </div>
-                <div class="w-16 h-16 bg-black rounded-full relative left-82 -top-10">
+                <div class="xl:w-16 xl:h-16 w-12 h-12 bg-black rounded-full relative xl:left-82 left-72 -top-10">
                     <a href={github} target="_blank">
-                        <img src={githubicon} alt={i+1} class="p-2">
+                        <img src={githubicon} alt={i+1} class="xl:p-2 p-1">
                     </a>
                 </div>
             </div>
@@ -27,6 +47,9 @@
 </section>
 
 <style>
+    #projects {
+        scroll-behavior: smooth;
+    }
     .projects-name {
         font-family: 'Jacques Francois', serif;
         font-size: 72px;
@@ -35,5 +58,10 @@
     .projects-data-name {
         font-family: 'Gurajada', sans-serif;
         font-size: 32px;
+    }
+    @media screen and (max-width: 640px) {
+        .projects-name {
+            font-size: 44px;
+        }
     }
 </style>
