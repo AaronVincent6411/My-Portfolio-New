257acf18a5c3affb0d6d404258935a888466f21a
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2024-01-28 16:13:26 +0530

    corrected mistake

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+	return {
+	  define: {
+		'import.meta.env': env
+	  },
 	plugins: [sveltekit()]
+	}
 });
