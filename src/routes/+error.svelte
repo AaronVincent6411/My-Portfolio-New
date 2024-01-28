cb4c8e59777f4f79b754098bd9b619de798c9608
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2024-01-28 14:15:54 +0530

    added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Navbar from "../components/Navbar.svelte";
+    import { fade } from "svelte/transition";
+    import { quintIn } from "svelte/easing";
+
+</script>
+
+
+<Navbar />
+
+<section id="error" class="w-full h-full top-16 relative sm:p-16 p-8 select-none flex flex-col text-center text-white" in:fade={{ easing:quintIn }} out:fade={{ easing:quintIn }}>
+    <h2 class="error-name">404</h2>
+    <h2>Sorry Lad, Page Not Found</h2>
+</section>
+
+<style>
+    h2 {
+        font-family: "Jacques Francois";
+        font-size: 72px;
+    }
+</style>
