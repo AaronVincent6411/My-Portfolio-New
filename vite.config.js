ff5ada903d4a4830bf541399cb00bf16903d1e18
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2024-01-28 16:15:17 +0530

    corrected mistake

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite'
 
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd(), '');
