4cdd8169bd08ad12171a5a4652ccb0b32faddb05
Aaron Vincent 6411 <aaronvincent6411@gmail.com>
2024-03-08 23:13:01 +0530

    corrected contact problem

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -69,7 +69,7 @@
   function sendEmail(e) {
     e.preventDefault();
 
-    emailjs.sendForm(import.meta.env.VITE_YOUR_SERVICE_ID, import.meta.env.VITE_YOUR_TEMPLATE_ID, e.target, import.meta.env.VITE_YOUR_API_KEY)
+    emailjs.sendForm(process.env.YOUR_SERVICE_ID, process.env.YOUR_TEMPLATE_ID, e.target, process.env.YOUR_API_KEY)
       .then((result) => {
         console.log(result.text);
         formRef.reset();
